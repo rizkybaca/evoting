e5cacd655268336da67e11e831943d24dbf985de
ta <akun.rizky.baca@gmail.com>
2020-12-18 20:57:30 +0700

    finish read vote and quick count

--- admin/kotak/data_kotak.php
+++ admin/kotak/data_kotak.php
@@ -1,1 +1,38 @@
-kotak
+<h3>Kotak Suara</h3>
+<br>
+<table border="1" style="border-collapse: collapse;">
+	<thead>
+		<tr>
+			<th>No</th>
+			<th>Kandidat</th>
+			<th>Pemilih</th>
+			<th>Waktu Memilih</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php 
+		$no=1;
+		$sql = $koneksi->query("SELECT * FROM tb_vote
+				JOIN tb_calon ON tb_vote.id_calon=tb_calon.id_calon
+				JOIN tb_pengguna ON tb_vote.id_pemilih=tb_pengguna.id_pengguna
+			");
+		while($data=$sql->fetch_assoc()) {
+		 ?>
+			<tr>
+				<td>
+					<?php echo $no++; ?>
+				</td>
+				<td>
+					<?php echo $data['nama_calon']; ?>
+				</td>
+				<td>
+					<?php echo $data['id_pemilih']; ?>
+				</td>
+				<td>
+					<?php echo $data['waktu']; ?>
+				</td>
+			</tr>
+		<?php 
+		} ?>
+	</tbody>
+</table>
--- admin/suara/data_suara.php
+++ admin/suara/data_suara.php
@@ -1,1 +1,44 @@
-data_suara
+<h3>Perolehan Suara</h3>
+<br>
+<table>
+	<thead>
+		<tr>
+			<th>No Urut</th>
+			<th>Nama Kandidat</th>
+			<th>Foto Kandidat</th>
+			<th>Jumlah Suara</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php 
+	$no=1;
+	$sql=$koneksi->query("SELECT * FROM tb_calon");
+	while ($data=$sql->fetch_assoc()){
+		$id_calon=$data["id_calon"];
+	?>
+		<tr>
+			<td>
+				<?php echo $data['id_calon']; ?>
+			</td>
+			<td>
+				<?php echo $data['nama_calon']; ?>
+			</td>
+			<td align="center">
+				<img src="foto/<?php echo $data['foto_calon']; ?>" width="150px" />
+			</td>
+			<td>
+				<h4>
+					<?php
+					$sql_hitung = "SELECT COUNT(id_vote) from tb_vote  where id_calon='$id_calon'";
+					$q_hit = mysqli_query($koneksi, $sql_hitung);
+					while ($row = mysqli_fetch_array($q_hit)) {
+						echo $row[0] . " Suara";
+					}
+					?>
+				</h4>
+			</td>
+		</tr>
+		<?php 
+		} ?>
+	</tbody>
+</table>

249467e8f7d595c5641cacedd364164e62fe5c4c
ta <akun.rizky.baca@gmail.com>
2020-12-23 20:47:21 +0700

    finish quick count-user

--- pemilih/suara/data_suara.php
+++ pemilih/suara/data_suara.php
@@ -1,12 +1,13 @@
 <?php 
 
-$calon=mysqli_query($koneksi, "SELECT nama_calon FROM tb_calon ORDER BY id_vote ASC");
-$suara=mysqli_query($koneksi, "SELECT COUNT(id_vote) FROM tb_calon WHERE id_calon ORDER BY id_vote ASC");
+$calon=mysqli_query($koneksi, "SELECT nama_calon FROM tb_calon ORDER BY id_calon ASC");
+$suara=mysqli_query($koneksi, "SELECT COUNT(id_vote) as s FROM tb_vote GROUP BY id_calon");
 ?>
 
 <h3>Quick Count</h3>
 <br>
 <br>
+
 <div style="width: 500px;height: 500px; background-color: #fff" align="center">
 		<canvas id="myChart"></canvas>
 </div>
@@ -18,7 +19,7 @@ $suara=mysqli_query($koneksi, "SELECT COUNT(id_vote) FROM tb_calon WHERE id_calo
 				labels: [<?php while ($p=mysqli_fetch_array($calon)){ echo '"'.$p['nama_calon'].'",';}?>],
 				datasets: [{
 					label: '# of Votes',
-					data: <?php while ($p=mysqli_fetch_array($suara)){ echo '"'.$p['suara'].'",';}?>],
+					data: [<?php while ($p=mysqli_fetch_array($suara)){ echo '"'.$p['s'].'",';}?>],
 					backgroundColor: [
 					'rgba(255, 99, 132, 0.2)',
 					'rgba(54, 162, 235, 0.2)'
